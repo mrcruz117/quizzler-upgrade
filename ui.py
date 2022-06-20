b95ac6344901608e39b86373af208ccc9b12bc81
mrcruz117 <mrcruz1989@gmail.com>
2022-06-20 10:22:00 -0400

    completed buttons

--- ui.py
+++ ui.py
@@ -20,5 +20,13 @@ class QuizInterface:
             fill=THEME_COLOR,
             font=("Arial", 20, "italic")
         )
-        self.canvas.grid(row=1, column=0, columnspan=2)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_image = PhotoImage(file="images/true.png")
+        false_image = PhotoImage(file="images/false.png")
+        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.false_button = Button(image=false_image)
+        self.true_button.grid(row=2, column=0)
+        self.false_button.grid(row=2, column=1)
+
         self.window.mainloop()
